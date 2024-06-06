01f3b375b3d814e0b27b78cf9d7f7e07ac9899cc
ISHIMWE ELIAB <97574791+ishimweeli@users.noreply.github.com>
2024-06-06 13:51:47 +0200

    Initial commit

---
+++ src/main/java/checks/currenttime/controller/TimeController.java
@@ -0,0 +1,18 @@
+package checks.currenttime.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class TimeController {
+
+    @GetMapping("/time")
+    public String getCurrentTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return "Current time is: " + now.format(formatter);
+    }
+}
